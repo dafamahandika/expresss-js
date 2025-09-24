9ce8add69a7c2c9a4c88c828f0097797476a5a1c
dafamahandika <dafamahandika@gmail.com>
2025-09-24 16:08:47 +0700

    connect ke db load balancing

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { Sequelize, DataTypes } = require("sequelize");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const app = express();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  logging: false, // matikan log query kalau nggak perlu
+});
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +28,32 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/hostname", async (req, res) => {
+  try {
+    const [results] = await sequelize.query("SELECT @@hostname AS hostname");
+    return res.status(200).json({
+      message: "Success",
+      hostname: results[0].hostname,
+    });
+  } catch (err) {
+    console.error("Query error:", err.message);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/count", async (req, res) => {
+  try {
+    const [results] = await sequelize.query("SELECT COUNT(*) AS total_user FROM users");
+    return res.status(200).json({
+      message: "Success",
+      total_user: results[0].total_user,
+    });
+  } catch (err) {
+    console.error("Query error:", err.message);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
 app.get("/", function (req, res) {
   const test = Math.floor(Math.random() * 10000);
   res.status(200).json({ message: "Server running", number: test });
@@ -25,4 +61,12 @@ app.get("/", function (req, res) {
 
 // app.use(require('./middlewares/midelware-error'))
 
-app.listen(6001, () => console.log("Listening on port: 6001 http://localhost:6001"));
+app.listen(6001, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connected to ProxySQL via Sequelize");
+    console.log(`Server running at http://localhost:${6001}`);
+  } catch (err) {
+    console.error("❌ Connection failed:", err.message);
+  }
+});
